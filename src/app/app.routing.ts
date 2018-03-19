25b2ffbea1cb0b3e604eb19ab4bbee0b2ae20c00
Oleksii Aleksandrov <oleksii.aleksandrov@valor-software.com>
2018-03-19 15:53:51 +0200

    Added lazy load

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,11 +2,21 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactsComponent } from './contacts/contacts.component';
-import { routes as childRoutes } from './home/home.routing';
+import { Page03Component } from './home/page-03/page-03.component';
+import { Page02Component } from './home/page-02/page-02.component';
+import { Page01Component } from './home/page-01/page-01.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
-  {path: 'home', component: HomeComponent, children: childRoutes},
+  {
+    path: 'home',
+    component: HomeComponent,
+    children: [
+      {path: '', redirectTo: 'page-01', pathMatch: 'full'},
+      {path: 'page-01', component: Page01Component},
+      {path: 'page-02', component: Page02Component},
+      {path: 'page-03', component: Page03Component}
+    ]},
   {path: 'about', component: AboutUsComponent},
   {path: 'contacts', component: ContactsComponent},
 ];
